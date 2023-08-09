fc0583b02501da994c9c53db7187276c25091854
Bertrand_GitHub <bertrand.meunier@protonmail.com>
2023-08-09 16:35:41 +0200

    Repository Creation

---
+++ src/main/java/bzh/eni/cinetek/CinetekApplication.java
@@ -0,0 +1,13 @@
+package bzh.eni.cinetek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinetekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinetekApplication.class, args);
+	}
+
+}
