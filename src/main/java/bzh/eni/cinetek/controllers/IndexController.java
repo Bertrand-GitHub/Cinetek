88f7ee91c52342538276003a14480b67a98abf0b
Bertrand_GitHub <bertrand.meunier@protonmail.com>
2023-08-10 14:42:41 +0200

    modif indexController

--- src/main/java/bzh/eni/cinetek/controllers/IndexController.java
+++ src/main/java/bzh/eni/cinetek/controllers/IndexController.java
@@ -1,5 +1,16 @@
 package bzh.eni.cinetek.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class IndexController {
 
+    @GetMapping({ "/", "/index"})
+    public String afficherIndex() {
+
+        return "index";
+    }
 }
