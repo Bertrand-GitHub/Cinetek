3aed4506f0c3bf7da19570e4f6ee875447bf2483
sdaniel2023 <daniel.sylvain@gmail.com>
2023-08-11 11:44:42 +0200

    afffichage du detail : modificaiton du controlleur detailsMovieController

--- src/main/java/bzh/eni/cinetek/controllers/detailsMovieController.java
+++ src/main/java/bzh/eni/cinetek/controllers/detailsMovieController.java
@@ -1,13 +1,17 @@
 package bzh.eni.cinetek.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class detailsMovieController {
 
-    @GetMapping({ "/detailsMovie"})
-    public String Insert() {
+    @GetMapping({ "/detailsMovie/{id}"})
+    public String Insert(@PathVariable int id) {
+        System.out.println("id" + id);
+
 
         return "detailsMovie";
     }
