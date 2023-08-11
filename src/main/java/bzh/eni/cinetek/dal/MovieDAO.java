a6fa379623d43317b00d3b91d4ae1712ea82deff
Bertrand_GitHub <bertrand.meunier@protonmail.com>
2023-08-11 16:32:06 +0200

    le dernier commit

--- src/main/java/bzh/eni/cinetek/dal/MovieDAO.java
+++ src/main/java/bzh/eni/cinetek/dal/MovieDAO.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public interface MovieDAO {
     public List<Movie> selectMovies();
+
     public void addMovie(Movie movie);
     public void updateMovie(Movie movie);
     public  void insertMovie(Movie movie);
